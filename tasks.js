e4572b2b1319b5bc0b84d6fd5fe6f7fe29f3646f
luismiguel <luismiguelrussot@hotmail.com>
2024-01-27 20:16:22 -0500

    add finished project

--- tasks.js
+++ tasks.js
@@ -1,8 +1,8 @@
 module.exports = (resources) => {
   const my = {}
   const shared = {
-    // SHARED_VAR: value
-    // ...
+    ERROR: 'Error',
+    PATH: '/v1/service/test/loadinfo'
   }
 
   // ============================================================================
@@ -12,40 +12,64 @@ module.exports = (resources) => {
     let output = {}
 
     try {
-      const data = await setup(await validate( await load(input)))
-      // business logic goes here...
-      // output = ...
-    } catch (e) { 
-      // if (e.message === ...
-      //   throw new Error('...
-      // }
-      // Default case
-      _debug(e.stack)
-      throw (e)
+      const data = await setup(await validate(await load(input)))
+      output = data.files + data.errored_files
+      s3_driver.add(shared.PATH, output)
+    } catch (e) {
+      throw new Error(JSON.stringify({
+        status: shared.ERROR,
+        messageError: e.message,
+      }))
     }
 
     return output
 
-    async function load(input={}) {
+    async function load(input = {}) {
       const config = {}
-      // config.var1 = input.var1
-      // ...
-      // config.config1 = await bundled_config.config('CONFIG1')
-      // ...
+      // assing imput object to config
+      config.files = input.Files
+      config.scanned_files = input.scanned_files
+      config.errored_files = input.errored_files
       return config
     }
-    
+
     async function setup(config) {
-      const data = {...config}
-      // ...
+      const data = { ...config }
+      // validated no ids repeate in scanned
+      data.files = await validationIfProcessed(data.files, data.scanned_files)
+      // validate length files elements to push in errors
+      for (let file of data.files) {
+        if (file.length < 15 || file.length > 19 || await alphanumericValidation(file) == true) {
+          data.errored_files.push(file)
+        }
+      }
+      // validated no ids repeate in errored
+      data.files = await validationIfProcessed(data.files, data.errored_files)
+
       return data
     }
 
     async function validate(config) {
-      ;[
-        // [variable, "display name"],
-      ].forEach(([item, name]) => { if (!item) { throw new Error( 'MissingInput: ' + name )}})
+      // valited inputs exist
+      if (!config.files) throw new Error('MissingInput: Files')
+      if (!config.scanned_files) throw new Error('MissingInput: scanned_files')
+      if (!config.errored_files) throw new Error('MissingInput: errored_files')
+      [
+        config.files
+      ].forEach(([item, name]) => { if (!item) { throw new Error('Files Empty') } })
       return config
+    }
+
+    async function validationIfProcessed(files, validate) {
+      for (let val of validate) {
+        files = files.filter(file => file != val);
+      }
+      return files;
+    }
+
+    async function alphanumericValidation(id) {
+      const regex = /[^\w\s]/;
+      return regex.test(id);
     }
   }
 
